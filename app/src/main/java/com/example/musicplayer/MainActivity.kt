f6643ec57e796394ab962d889fcd7b47754ebeaf
82109 <uyfly@naver.com>
2023-03-27 00:09:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.musicplayer
+
+import android.content.Intent
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.musicplayer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playButton.setOnClickListener {
+            mediaPlayerPlay()
+        }
+
+        binding.pauseButton.setOnClickListener {
+            mediaPlayerPause()
+        }
+
+        binding.stopButton.setOnClickListener {
+            mediaPlayerStop()
+        }
+    }
+
+    private fun mediaPlayerPlay() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_PLAY }
+        startService(intent)
+
+//        if (mediaPlayer == null) {
+//            mediaPlayer = MediaPlayer.create(this, R.raw.prepare_to_be_boarded).apply {
+//                isLooping = true
+//            }
+//        }
+//        mediaPlayer?.start()
+    }
+
+    private fun mediaPlayerPause() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_PAUSE }
+        startService(intent)
+//        mediaPlayer?.pause()
+    }
+
+    private fun mediaPlayerStop() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_STOP }
+        startService(intent)
+//        mediaPlayer?.stop()
+//        mediaPlayer?.release()
+//        mediaPlayer = null
+    }
+
+    override fun onDestroy() {
+        stopService(Intent(this, MediaPlayerService::class.java))
+        super.onDestroy()
+    }
+}
---
+++ app/src/main/java/com/example/musicplayer/MediaPlayerService.kt
@@ -0,0 +1,145 @@
+package com.example.musicplayer
+
+import android.app.*
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.drawable.Icon
+import android.media.MediaPlayer
+import android.os.IBinder
+
+class MediaPlayerService : Service() {
+
+    private var mediaPlayer: MediaPlayer? = null
+    private val receiver = LowBatteryReceiver()
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+        initReceiver()
+
+        val playIcon = Icon.createWithResource(baseContext, R.drawable.baseline_play_arrow_24)
+        val pauseIcon = Icon.createWithResource(baseContext, R.drawable.baseline_pause_24)
+        val stopIcon = Icon.createWithResource(baseContext, R.drawable.baseline_stop_24)
+
+        val mainPendingIntent = PendingIntent.getActivity(
+            baseContext,
+            0,
+            Intent(baseContext, MainActivity::class.java)
+                .apply {
+                    flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+                },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val pausePendingIntent = PendingIntent.getService(
+            baseContext,
+            0,
+            Intent(baseContext, MediaPlayerService::class.java).apply {
+                action = MEDIA_PLAYER_PAUSE
+            },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val playPendingIntent = PendingIntent.getService(
+            baseContext,
+            0,
+            Intent(baseContext, MediaPlayerService::class.java).apply {
+                action = MEDIA_PLAYER_PLAY
+            },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val stopPendingIntent = PendingIntent.getService(
+            baseContext,
+            0,
+            Intent(baseContext, MediaPlayerService::class.java).apply {
+                action = MEDIA_PLAYER_STOP
+            },
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val notification = Notification.Builder(baseContext, CHANNEL_ID)
+            .setStyle(
+                Notification.MediaStyle()
+                    .setShowActionsInCompactView(0, 1, 2)
+            )
+            .setVisibility(Notification.VISIBILITY_PUBLIC)
+            .setSmallIcon(R.drawable.baseline_star_24)
+            .addAction(
+                Notification.Action.Builder(
+                    pauseIcon,
+                    "Pause",
+                    pausePendingIntent
+                ).build()
+            )
+            .addAction(
+                Notification.Action.Builder(
+                    playIcon,
+                    "Play",
+                    playPendingIntent
+                ).build()
+            )
+            .addAction(
+                Notification.Action.Builder(
+                    stopIcon,
+                    "Stop",
+                    stopPendingIntent
+                ).build()
+            )
+            .setContentIntent(mainPendingIntent)
+            .setContentTitle("음악재생")
+            .setContentText("음원이 재생 중 입니다..")
+            .build()
+
+        startForeground(100, notification)
+    }
+
+    private fun initReceiver() {
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+        registerReceiver(receiver, filter)
+    }
+
+    private fun createNotificationChannel() {
+        val channel =
+            NotificationChannel(CHANNEL_ID, "MEDIA_PLAYER", NotificationManager.IMPORTANCE_DEFAULT)
+
+        val notificationManager = baseContext.getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.action) {
+            MEDIA_PLAYER_PLAY -> {
+                if (mediaPlayer == null) {
+                    mediaPlayer = MediaPlayer.create(baseContext, R.raw.prepare_to_be_boarded)
+                }
+                mediaPlayer?.start()
+            }
+            MEDIA_PLAYER_PAUSE -> {
+                mediaPlayer?.pause()
+            }
+            MEDIA_PLAYER_STOP -> {
+                mediaPlayer?.stop()
+                mediaPlayer?.release()
+                mediaPlayer = null
+                stopSelf()
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        mediaPlayer?.apply {
+            stop()
+            release()
+        }
+        mediaPlayer = null
+        unregisterReceiver(receiver)
+        super.onDestroy()
+    }
+}
